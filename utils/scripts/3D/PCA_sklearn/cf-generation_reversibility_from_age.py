062f19bcb75e904ef4000e6371495cd520ecdd55
erikohara <erik_yo@hotmail.com>
2025-05-20 13:53:42 -0600

    step4

--- utils/scripts/3D/PCA_sklearn/cf-generation_reversibility_from_age.py
+++
@@ -1,5 +0,0 @@
-
-
-input_image = '/work/forkert_lab/erik/MACAW/cf_images/2_5Z_65'
-output_image = '/work/forkert_lab/erik/MACAW/cf_images/2_5Z_reversability_from_age'
-ukbb_path = '/home/erik.ohara/UKBB'
